f609b68f199019a8fd661ebbb0128d38f18a4f19
unknown-user <unknown-user@10.0.1.61>
2015-04-27 22:34:04 -0500

    Initial check-in of module RestCallProject

---
+++ RestCallProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using RestSharp;
+
+
+namespace RestCallProject
+{
+	public class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var client = new RestClient("https://www.quandl.com/api/v1/datasets/ODA/USA_NGDP.json");
+			var request = new RestRequest(Method.GET);
+			request.AddHeader("auth_token","UYA5shpxWr8m-GsVbdeU");
+			//request.RequestFormat = DataFormat.Json;
+			IRestResponse response = client.Execute (request);
+			Console.WriteLine("Response from the Website");
+			Console.WriteLine(response.Content);
+		}
+	}
+}
+
+
+
+			
